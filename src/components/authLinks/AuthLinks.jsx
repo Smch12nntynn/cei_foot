ccbf5acb6ad31ebbf620e70f59985028609853cd
michel <mixmarques99@gmail.com>
2024-06-24 21:29:03 -0300

    navgation

---
+++ src/components/authLinks/AuthLinks.jsx
@@ -0,0 +1,43 @@
+"use client"
+import React, { useState } from 'react'
+import styles from "./authLinks.module.css"
+import Link from 'next/link'
+
+const AuthLinks = () => {
+    const status = "notauthenticated"
+    const [open, setOpen] = useState(false)
+    return (
+        <>
+            {status === "notauthenticated" ? (
+                <Link href="/login">Login</Link>
+            ) : (
+                <>
+                    <Link href="/write">Escrever</Link>
+                    <span className={styles.link}>Logout</span>
+                </>
+            )}
+            <div className={styles.burger} onClick={() => setOpen(!open)}>
+                <div className={styles.line}></div>
+                <div className={styles.line}></div>
+                <div className={styles.line}></div>
+            </div>
+            {open && (
+                <div className={styles.responsiveMenu}>
+                    <Link href="/">Homepage</Link>
+                    <Link href="/">Sobre</Link>
+                    <Link href="/">Contato</Link>
+                    {status === "notauthenticated" ? (
+                        <Link href="/login">Login</Link>
+                    ) : (
+                        <>
+                            <Link href="/write">Escrever</Link>
+                            <span className={styles.link}>Logout</span>
+                        </>
+                    )}
+                </div>
+            )}
+        </>
+    )
+}
+
+export default AuthLinks
