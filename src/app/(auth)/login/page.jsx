86744f86fe90c42b3f1ef1c39ac0558b46c36138
michel <mixmarques99@gmail.com>
2024-06-29 13:51:07 -0300

    design login

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import styles from './login.module.css'
 
-const loginPage = () => {
+const page = () => {
     return (
-        <div>loginPage</div>
+        <div className={styles.container}>
+            <div className={styles.wrapper}>
+                <div className={styles.socialButton}>Sign in with Google</div>
+                <div className={styles.socialButton}>Sign in with Facebook</div>
+            </div>
+        </div>
     )
 }
 
-export default loginPage
+export default page
